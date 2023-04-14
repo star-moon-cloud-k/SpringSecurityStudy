c1f770dc021d9fe70f28199efea357818c0ead8d
star-moon-cloud-k <kspecial44@gmail.com>
2023-04-14 16:58:21 +0900

    메소드 필터링

--- jwt/src/main/java/com/example/jwt/config/FilterConfig.java
+++ jwt/src/main/java/com/example/jwt/config/FilterConfig.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
-    @Bean
-    public FilterRegistrationBean<MyFilter1> filter1(){
-        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
-        bean.addUrlPatterns("/*");
-        bean.setOrder(0);       //낮은 번호가 필터 중 가장 먼저 실행됨
-        return bean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<MyFilter1> filter1(){
+//        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+//        bean.addUrlPatterns("/*");
+//        bean.setOrder(0);       //낮은 번호가 필터 중 가장 먼저 실행됨
+//        return bean;
+//    }
 
     @Bean
     public FilterRegistrationBean<MyFilter2> filter2(){
--- jwt/src/main/java/com/example/jwt/filter/MyFilter1.java
+++ jwt/src/main/java/com/example/jwt/filter/MyFilter1.java
@@ -2,12 +2,37 @@ package com.example.jwt.filter;
 
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter1 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("필터1");
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+
+        /**
+         * Id, PW가 정상적으로 들어와서 로그인이 되는 경우 토큰을 만들어주고 응답해줌
+         * 요청할 떄 마다 header에 Authorization 에서 value 값으로 토큰을 던져줌
+         * 그 때 토큰이 넘어오면 토큰이 적절한 토큰인지 검증 (RSA , HS256)
+         */
+        if(req.getMethod().equals("POST")){
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+            if(headerAuth.equals("hello")){
+                filterChain.doFilter(req,res);
+            }else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");       //인증이 안된 경우 return 해줌 필터에서 체이닝이 끊어지고 차단됨.
+            }
+
+        }
+
+//        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
