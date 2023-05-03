aa64810bbc03455f0f11c9ebd4109457436e1378
star-moon-cloud-k <kspecial44@gmail.com>
2023-05-03 21:02:00 +0900

    jwt 토큰 필터

--- jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.jwt.config;
 
 import com.example.jwt.config.filter.MyFilter1;
 import com.example.jwt.config.jwt.JwtAuthenticationFilter;
+import com.example.jwt.config.jwt.JwtAuthorizationFilter;
+import com.example.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,9 +19,10 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsFilter corsFilter;
+    private final UserRepository userRepository;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new MyFilter1(),BasicAuthenticationFilter.class);      //BasicAuthenticationFilter 필터보다 Myfilter1 필터가 먼저 실행되게 실행
+//        http.addFilterBefore(new MyFilter1(),BasicAuthenticationFilter.class);      //BasicAuthenticationFilter 필터보다 Myfilter1 필터가 먼저 실행되게 실행
 //        http.addFilterBefore(new MyFilter2(),BasicAuthenticationFilter.class);        //Ek
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)    //세션을 사용하지 않음
@@ -27,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(corsFilter)      //@CrossOrigin(인증x) , 시큐리티 필터에 등록 (인증O)
                 .formLogin().disable()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))   //formlogin 방식 대신 jwt 토큰활용하여 필터링 AuthenticationManager 가 필요함 _WebSecurityConfigurerAdapter가 가지고있음
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),userRepository))
                 .httpBasic().disable()      //http 기본 인증방식을 사용하지 않음
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
